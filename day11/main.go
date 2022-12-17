4c5d7f9de5b37bc7e94cd4be363d91d01c650d0a
bcutler94 <bcutler94@gmail.com>
2022-12-17 10:47:34 -0500

    fuck day11

--- day11/main.go
+++ day11/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/parser"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -10,26 +13,70 @@ func main()  {
 	BuildGaggle()
 }
 
-Starting items: 
-
 type Monkey struct {	
-	id    byte
+	id    int
 	items []int
-	operation string
-	factor int
+	operation ast.Expr
+	divisible int
+	divTrue int
+	divFalse int
 }
 
 func (monkey *Monkey) Init(input string) {
-	arr := strings.Split(input, "/n")
+	arr := strings.Split(input, "\n")
 
 	// ID
 	id := arr[0][len(arr[0]) - 2]
-	monkey.id = id
+	monkey.id = int(id)
 
-	// 
-	strItems := strings.Split(arr[1][15:], ",")
-	
+	// Starting items
+	strItems := strings.Split(arr[1][18:], ", ")
+	startingItems := []int{}
+	for _, item := range strItems {
+		intItem, err := strconv.Atoi(item)
+		if err != nil {
+			panic(err)
+		}
+		startingItems = append(startingItems, intItem)
+	}
+	monkey.items = startingItems
 
+	// Operation
+	strExpr := strings.Split(arr[2], "=")
+	expr, err := parser.ParseExpr(strExpr[1])
+	if err != nil {
+		panic(err)
+	}
+	monkey.operation = expr
+
+	// Divisble
+	strDivisble := strings.Split(arr[3], " ")
+	divisible, err := strconv.Atoi(strDivisble[len(strDivisble) - 1])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Div", divisible)
+	monkey.divisible = divisible
+
+	// DivTrue
+	strDivs := strings.Split(arr[4], " ")
+	divTrue, err := strconv.Atoi(strDivs[len(strDivs) - 1])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("DivTrue", divTrue)
+	monkey.divTrue = divTrue
+
+	// DivFalse
+	strDivsF := strings.Split(arr[5], " ")
+	divFalse, err := strconv.Atoi(strDivsF[len(strDivsF) - 1])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("DivFalse", divFalse, strDivsF)
+	monkey.divFalse = divFalse
+
+	fmt.Println("Created monkey", monkey)
 }
 
 type Gaggle map[int]*Monkey
@@ -38,7 +85,7 @@ func (gaggle *Gaggle) AddMonkey(monkey *Monkey) {
  (*gaggle)[monkey.id] = monkey
 }
 
-func BuildGaggle() *Gaggle {
+func BuildGaggle() Gaggle {
 	file, err := os.ReadFile("./input.txt")
 	if err != nil {
 		panic(err)
@@ -46,13 +93,12 @@ func BuildGaggle() *Gaggle {
 
 	fileStr := string(file)
 	arr := strings.Split(fileStr, "\n\n")
-	gaggle := new(Gaggle)
+	gaggle := make(Gaggle)
 	for _, item := range arr {
 		monkey := new(Monkey)
 		monkey.Init(item)
 		gaggle.AddMonkey(monkey)
 	}
 
-	fmt.Println(arr, len(arr))
 	return gaggle
 }
